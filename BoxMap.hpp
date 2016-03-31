bbab899f863e326162397adb973784586551699a
Sander <sanderkolman@gmail.com>
2016-03-31 13:38:55 +0200

    Added code-skeleton, no functions implemented yet

---
+++ BoxMap.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "MapInterface.hpp"
+
+namespace r2d2{
+	namespace map{
+		class BoxMap : public SaveLoadMap, ReadWriteMap, ReadOnlyMap{
+		public:
+
+			BoxMap::BoxMap();
+
+			BoxInfo & ReadOnlyMap::get_box_info(const adt::Box & box) override;
+			adt::Box & ReadOnlyMap::get_map_bounding_box() override;
+			std::shared_ptr<BoxInfo**> ReadOnlyMap::get_map_area_2d(const adt::Box & area, const adt::Box & pixel_size) override;
+
+			void ReadWriteMap::set_box_info(const adt::Box & box, const BoxInfo & box_info) override;
+
+			void SaveLoadMap::save(const std::string filename) override;
+			void SaveLoadMap::load(const std::string filename) override;
+
+		private:
+			std::vector<std::pair<adt::Box, BoxInfo>> map;
+		};
+	}
+}
---
+++ MapInterface.cpp
@@ -0,0 +1,21 @@
+#include "MapInterface.hpp"
+
+namespace r2d2{
+	namespace map{
+		BoxInfo::BoxInfo(const bool has_obstacle, const bool has_unknown, const bool has_navigatable) :
+			has_obstacle{ has_obstacle },
+			has_unknown{ has_unknown },
+			has_navigatable{ has_navigatable }
+		{}
+
+      bool BoxInfo::operator==(const BoxInfo & lhs) const
+      {
+          return has_obstacle == lhs.has_obstacle && has_unknown == lhs.has_unknown && has_navigatable == lhs.has_navigatable;
+      }
+
+      bool BoxInfo::operator!=(const BoxInfo & lhs) const
+      {
+          return !operator==(lhs);
+      }
+	}
+}
