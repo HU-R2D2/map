8268bf957a19a50db46cfc3208f6ba3f4080593b
naptex <naptechxx@live.nl>
2016-04-04 15:51:20 +0200

    created directory structure

---
+++ source/include/BoxMap.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "MapInterface.hpp"
+
+namespace r2d2
+{
+    namespace map
+    {
+        class BoxMap : public SaveLoadMap, ReadWriteMap, ReadOnlyMap
+        {
+
+        public:
+
+            BoxMap::BoxMap();
+            
+            BoxInfo & ReadOnlyMap::get_box_info(const Box & box) override;
+            Box & ReadOnlyMap::get_map_bounding_box() override;
+            std::shared_ptr<BoxInfo**> ReadOnlyMap::get_map_area_2d(const Box & area, const Box & pixel_size) override;
+
+            void ReadWriteMap::set_box_info(const Box & box, const BoxInfo & box_info) override;
+
+            void SaveLoadMap::save(const std::string filename) override;
+            void SaveLoadMap::load(const std::string filename) override;
+
+
+        private:
+            std::vector<std::pair<Box, BoxInfo>> map;
+
+        };
+    }
+}
---
+++ source/src/MapInterface.cpp
@@ -0,0 +1,23 @@
+#include "../include/MapInterface.hpp"
+
+namespace r2d2
+{
+    namespace map
+    {
+        BoxInfo::BoxInfo(const bool has_obstacle, const bool has_unknown, const bool has_navigatable) :
+            has_obstacle{ has_obstacle },
+            has_unknown{ has_unknown },
+            has_navigatable{ has_navigatable }
+        {}
+
+        bool BoxInfo::operator==(const BoxInfo & rhs) const
+        {
+            return has_obstacle == rhs.has_obstacle && has_unknown == rhs.has_unknown && has_navigatable == rhs.has_navigatable;
+        }
+
+        bool BoxInfo::operator!=(const BoxInfo & rhs) const
+        {
+            return !operator==(rhs);
+        }
+    }
+}
