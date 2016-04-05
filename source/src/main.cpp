4305570954aca7eb7527e6db8ce33092fa84583e
anasshehata <anasshehata@fastmail.fm>
2016-04-05 14:02:10 +0200

    added main.cpp

---
+++ source/src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "MapInterface.hpp"
+#include "BoxMap.hpp"
+
+
+
+int main(){
+
+
+
+	/** Operator== and Operator!= Examples
+	r2d2::map::BoxInfo boxA{true,true,true};
+	r2d2::map::BoxInfo boxB{true,true,true};
+	r2d2::map::BoxInfo boxC{false,true,true};
+	r2d2::map::BoxInfo boxD{false,false,true};
+	bool AEqualsB = boxA == boxB;
+	bool ANotEqualsB = boxA != boxB;
+	bool CEqualsD = boxC == boxD;
+	bool CNotEqualsD = boxC != boxD;
+
+	/**std::cout << " A==B - should return true :" << std::boolalpha <<AEqualsB << " \n ";
+	std::cout << "A!=B - should return false :" << std::boolalpha << ANotEqualsB << " \n ";
+	std::cout << "C==D - should return false :" << std::boolalpha << CEqualsD << " \n ";
+	std::cout << "C!=D - should return true :" << std::boolalpha << CNotEqualsD << " \n ";**/
+	system("PAUSE");
+
+	return 0;
+}
