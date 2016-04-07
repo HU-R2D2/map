b965efb55cce0a0f60fab4d0d42aaba3088443c5
MaikelB <maikel.bolderdijk@student.hu.nl>
2016-04-07 12:01:04 +0200

    Initial implementation of load()

--- source/src/MapInterface.cpp
+++ source/src/MapInterface.cpp
@@ -1,6 +1,8 @@
 
 #include "../../../adt/source/include/Box.hpp"
 #include "../include/MapInterface.hpp"
+#include "../include/rapidjson/document.h"
+#include "../include/rapidjson/filereadstream.h"
 
 namespace r2d2
 {
@@ -29,4 +31,23 @@ namespace r2d2
     bool BoxInfo::get_has_navigatable(){
         return BoxInfo::has_navigatable;
     }
+
+    void SaveLoadMap::Save(std::string filename) {
+
+    }
+
+    void SaveLoadMap::Load(std::string filename) {
+        FILE* pFILE = fopen("test.json" , "rb");
+        char buff[65536];
+        rapidjson::FileReadStream is(pFILE, buff, sizeof(buff));
+        // 1. Parse a JSON string into DOM
+        rapidjson::Document d;
+        d.ParseStream<0, rapidjson::UTF8<>, rapidjson::FileReadStream>(is);
+
+        Box::Box pBox = Box(Coordinate(d["box"]["leftCoordinate"]["x"].GetDouble() * Length::METER, d["box"]["leftCoordinate"]["y"].GetDouble() * Length::METER, d["box"]["leftCoordinate"]["z"].GetDouble() * Length::METER), Coordinate::Coordinate(d["box"]["rightCoordinate"]["x"].GetDouble() * Length::METER, d["box"]["rightCoordinate"]["y"].GetDouble() * Length::METER, d["box"]["rightCoordinate"]["z"].GetDouble() * Length::METER));
+        BoxInfo::Boxinfo pBoxInfo = BoxInfo::BoxInfo(d["box"]["boxInfo"]["has_obstacle"].GetBool(),d["box"]["boxInfo"]["has_unknown"].GetBool(),d["box"]["boxInfo"]["has_navigatable"].GetBool());
+        map.set_box_info(pBox, pBoxInfo);
+    }
+
 }
+
