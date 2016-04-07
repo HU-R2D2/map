95d690c3f7a7eac3851aff020c36c0c989e75229
Sander <sanderkolman@gmail.com>
2016-04-07 17:41:04 +0200

    checked columncount on MapInterface.cpp

--- source/src/MapInterface.cpp
+++ source/src/MapInterface.cpp
@@ -4,7 +4,11 @@
 
 namespace r2d2
 {
-    BoxInfo::BoxInfo(bool has_obstacle, bool has_unknown, bool has_navigatable) :
+    BoxInfo::BoxInfo(
+        bool has_obstacle,
+        bool has_unknown,
+        bool has_navigatable
+    ):
         has_obstacle{ has_obstacle },
         has_unknown{ has_unknown },
         has_navigatable{ has_navigatable }
@@ -12,7 +16,11 @@ namespace r2d2
 
     bool BoxInfo::operator==(const BoxInfo rhs) const
     {
-        return has_obstacle == rhs.has_obstacle && has_unknown == rhs.has_unknown && has_navigatable == rhs.has_navigatable;
+        return (
+            has_obstacle == rhs.has_obstacle && 
+            has_unknown == rhs.has_unknown && 
+            has_navigatable == rhs.has_navigatable
+        );
     }
 
     bool BoxInfo::operator!=(const BoxInfo rhs) const
