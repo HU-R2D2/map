fda1264f5fff387be1a8df1d937681eb0dbe89b4
chieltbest <chieltbest@gmail.com>
2016-06-14 14:17:16 +0200

    added default boxmap implementation header, so that implementations can be switched easily

---
+++ source/include/DefaultBoxMap.hpp
@@ -0,0 +1,16 @@
+//
+// Created by chiel on 14/06/16.
+//
+
+#ifndef MAP_DEFAULTBOXMAP_HPP
+#define MAP_DEFAULTBOXMAP_HPP
+
+#include "ArrayBoxMap.hpp"
+
+namespace r2d2 {
+
+	typedef ArrayBoxMap DefaultBoxMap;
+
+}
+
+#endif //MAP_DEFAULTBOXMAP_HPP
